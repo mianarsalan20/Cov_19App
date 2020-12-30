54052ed6d2bda96192c442e8b5d9c06da89f3269
Muhammad Arsalan <mianarsalan20@gmail.com>
2020-12-31 03:24:29 +0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button, TouchableOpacity,Navigation,NavigatorIOS, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+import WorldStatistic from './components/WorldStatistic';
+import countriesList from './components/countriesList';
+import favourite from './components/favourite';
 
-export default function App() {
+function MyDrawer() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Drawer.Navigator>
+      <Drawer.Screen name="WorldStatistic" component={stackNavigator} />
+      <Drawer.Screen name="CountriesList" component={countriesList} />
+      <Drawer.Screen name="Favourite" component={stackNavigator} />
+    </Drawer.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const stackNavigator = () => {
+  return (
+  <Stack.Navigator //initialRouteName={'Home'}
+      screenOptions={({navigation}) => ({
+        headerLeft: () =>
+        <Ionicons name="md-menu" size={24} color="black" onPress = { () => navigation.openDrawer() } />
+      })
+  }>
+        <Stack.Screen name="home" component={WorldStatistic} />
+        <Stack.Screen name="countriesList" component={countriesList} 
+        options={({navigation}) => ({
+        headerLeft: () =>
+        <Ionicons name="arrow-back-circle-sharp" size={30} color="black" onPress = { () => navigation.goBack() } />
+      })
+  } />
+  </Stack.Navigator>
+  );
+}
+
+
+export default function App({navigation}){
+  return(
+    <NavigationContainer>
+      <MyDrawer/>
+    </NavigationContainer>
+  )
+}
---
+++ components/favourite.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { 
+  Text,
+  View,
+  StyleSheet, 
+  Button, 
+  TouchableOpacity,
+  Navigation,
+  NavigatorIOS,
+  TextComponent} from 'react-native';
+
+
+
+ export default function  StartScreen ({navigation}) {
+ 
+  return (
+    <View style={styles.container}>
+
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText} >WORLD</Text>
+        <Text style={styles.subtitleText} >STATISTICS</Text>
+      </View>
+    
+
+      <View style={{paddingTop: 50}}>
+        <TouchableOpacity
+          style={styles.appButtonContainer} onPress={() => navigation.navigate('countriesList')}>
+           
+          <Text style={styles.appButtonText} >Countries List</Text>
+        </TouchableOpacity>
+      </View>
+      
+    </View>
+  );
+}
+ 
+ const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  titleContainer: {
+    backgroundColor: 'black',
+    width: '100%',
+    padding: 3,
+  },
+  titleText: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  subtitleText: {
+    fontSize: 20,
+    color: 'white',
+    alignSelf: 'center',
+  },
+
+  appButtonContainer: {
+    backgroundColor: 'black',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+  appButtonText: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  
+});
